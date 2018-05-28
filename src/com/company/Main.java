1e1b79a37315e94cfffea376250cacb6926fc988
Shafiq Elaradi <shafik93@icloud.com>
2018-05-28 05:24:11 +0200

    SocketServer

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+
+
+//        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(4444);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+
+
+            String inputLine, outputLine;
+
+
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+
+
+        }catch (IOException e){
+
+        }
+
+    }
+
+
+}
